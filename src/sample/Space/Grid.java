159cd39a3bfeb3edb73e8fae13da7a95f3264a89
Nils Rasamoel <nils.rasamoel@gmail.com>
2019-04-03 16:23:30 +0200

    Fixed some scaling issues

--- src/sample/Space/Grid.java
+++ src/sample/Space/Grid.java
@@ -2,7 +2,7 @@ package sample.Space;
 
 public class Grid {
     final float WIDTH, HEIGHT;
-    Vector2D scale;
+    Vector2D scale, offset;
 
     public Grid(float width, float height){
         this.WIDTH = width;
@@ -12,12 +12,18 @@ public class Grid {
     public void setScale(Vector2D topLeft, Vector2D topRight, Vector2D bottomRight, Vector2D bottomLeft){
         float w, h;
         w = (topRight.getX() + bottomRight.getX() - (topLeft.getX() + bottomLeft.getX()))/2f;
-        h = (topRight.getY() + topLeft.getY() - (bottomLeft.getY() + bottomRight.getY()))/2f;
+        h = (bottomRight.getY() + bottomLeft.getY() - (topLeft.getY() + topRight.getY()))/2f;
 
         scale = new Vector2D(WIDTH/w, HEIGHT/h);
+        offset = new Vector2D(
+                (topLeft.getX() + bottomLeft.getX())/2f,
+                (topRight.getY() + topLeft.getY())/2f
+                );
     }
 
     public Vector2D translatePos(Vector2D pos){
-        return pos.scale(scale);
+        return pos
+                .subtract(offset)
+                .scale(scale);
     }
 }
